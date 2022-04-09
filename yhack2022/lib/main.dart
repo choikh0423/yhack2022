751bcd9531d0b39a382b509902f4b03dc69c55ad
choikh0423 <kyuhwanchoi0423@gmail.com>
2022-04-10 08:17:18 +0900

    suggestion

--- yhack2022/lib/main.dart
+++ yhack2022/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'suggestions.dart';
 import 'search.dart';
 
-void main() => runApp(const MyApp());
-
-FirebaseFirestore firestore = FirebaseFirestore.instance;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
--- yhack2022/lib/suggestions.dart
+++ yhack2022/lib/suggestions.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class suggestions extends StatefulWidget {
   const suggestions({Key? key}) : super(key: key);
@@ -19,57 +21,102 @@ class _suggestionsState extends State<suggestions> {
             settings: settings,
             builder: (BuildContext context) {
               return Scaffold(
-                appBar: AppBar(title: Text("Sort"),
+                appBar: AppBar(
+                  title: Text("Sort"),
                   leading: GestureDetector(
-                    onTap: (){},
+                    onTap: () {},
                     child: Icon(Icons.sort),
                   ),
                 ),
-                body: ListView(
-                  padding: EdgeInsets.all(20),
-                  children: <Widget>[
-                    Container(
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Text("textfield 1: input date"),
-                              Spacer(),
-                              Icon(Icons.keyboard_arrow_right)
-                            ],
-                          ),
-                          Text("textfield 2: input content"),
-                          Text("textfield 3: input SNS type"),
-                          Text("textfield 4: input type of content: Post or Comment"),
-                          Text("textfield 5: input danger_filter")
-                        ])),
-                    GestureDetector(
-                      onTap:(){
-
-                      }
-                    ),
-                    Container(
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                          Text("textfield 1"),
-                          Text("textfield 2"),
-                          Text("textfield 3")
-                        ])),
-                    Container(
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                          Text("textfield 1"),
-                          Text("textfield 2"),
-                          Text("textfield 3")
-                        ])),
-                  ],
-                ),
+                body: ContentList(),
                 backgroundColor: Colors.white,
               );
             });
+      },
+    );
+  }
+}
+
+class ContentList extends StatefulWidget {
+  const ContentList({Key? key}) : super(key: key);
+
+  @override
+  State<ContentList> createState() => _ContentListState();
+}
+
+class _ContentListState extends State<ContentList> {
+  Future<QuerySnapshot> get_documents() async {
+    final list = [];
+    return await FirebaseFirestore.instance
+        .collection('content')
+        .where('danger', arrayContainsAny: ['Profanity'])
+        .where('type', isEqualTo: "Post")
+        .get();
+  }
+
+  array_to_string(arr) {
+    String str = "";
+    for (String word in arr) {
+      str = str + " " + word + ",";
+    }
+    return str;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: get_documents(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            padding: EdgeInsets.all(20),
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                // onTap: () {
+                //   _link(context);
+                // },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 5),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                          color: Colors.black,
+                          style: BorderStyle.solid,
+                          width: 1.0),
+                    ),
+                  ),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(snapshot.data!.docs[index]['date_created']),
+                            Spacer(),
+                            Icon(
+                              Icons.keyboard_arrow_right,
+                            ),
+                          ],
+                        ),
+                        Text("textfield 2: " +
+                            snapshot.data!.docs[index]['content']),
+                        Text("textfield 3: " +
+                            snapshot.data!.docs[index]['sns']),
+                        Text("textfield 4: " +
+                            snapshot.data!.docs[index]['type']),
+                        Text("textfield 5: input_danger_filter: " +
+                            array_to_string(
+                                snapshot.data!.docs[index]['danger'])),
+                      ]),
+                ),
+              );
+            },
+          );
+        } else if (snapshot.connectionState == ConnectionState.none &&
+            snapshot.hasData == null) {
+          return Text("No data");
+        }
+        return Center(child: CircularProgressIndicator());
       },
     );
   }
