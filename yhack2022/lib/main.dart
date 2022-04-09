c082e30b13d4e626b7f12965fb7e4365e8ad9d4e
choikh0423 <kyuhwanchoi0423@gmail.com>
2022-04-09 23:29:51 +0900

    merge conflict on main.dart

--- yhack2022/lib/main.dart
+++ yhack2022/lib/main.dart
@@ -1,84 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'suggestions.dart';
+import 'search.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const HomePage());
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: homepage());
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class homepage extends StatefulWidget {
+  const homepage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<homepage> createState() => _homepageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _homepageState extends State<homepage> {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: GetContent(),
-    );
+  int _selectedIndex = 0;
+
+  static const List<Widget> _appBarTitle = <Widget>[
+    Text('Suggestions'),
+    Text('Search')
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
-}
 
-class AddContent extends StatelessWidget {
-  const AddContent({Key? key}) : super(key: key);
-
-  final String sns = "";
-  final String type = "";
-  final String url = "";
-  final String content = "";
-
-  @override
-  Widget build(BuildContext context) {
-    CollectionReference contents =
-        FirebaseFirestore.instance.collection('content');
-
-    Future<void> addContent() {
-      return contents
-          .add({
-            'Sns': sns,
-            'Type': type,
-            'Url': url,
-            'Content': content,
-          })
-          .then((value) => print("Content Added"))
-          .catchError((error) => print("Failed to add content: $error"));
-    }
-
-    return TextButton(onPressed: addContent, child: Text("Add Content"));
-  }
-}
-
-class GetContent extends StatelessWidget {
-  const GetContent({Key? key}) : super(key: key);
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(title: Text('Demo')),
-      body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('content').snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return Text("Loading Data");
-            return Column(children: <Widget>[
-              Text(snapshot.data.documents[0]['contents']?),
-            ]);
-          }),
+      appBar: AppBar(
+          title: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: _appBarTitle[_selectedIndex],
+          )
+        ],
+      )),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [suggestions(), search()],
+      ),
+      bottomNavigationBar: Container(
+        //decoration
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.blue,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.lightbulb), label: "suggestions"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "search"),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
+      ),
     );
   }
 }
